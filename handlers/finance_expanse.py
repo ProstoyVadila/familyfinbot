7ed2c25b6080d03e48bae340169ce2f9e05a617a
gorbachev <vadim.gorbachev.main@yandex.ru>
2020-12-17 00:04:08 +0300

    added finance handler

---
+++ handlers/finance_expanse.py
@@ -0,0 +1,45 @@
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+
+from app import dp, bot
+from messages import base, error
+from keyboards.finance_keyboard import expanse_category_keyboard, menu_keyboard
+from states.finance_states import ExpanseState
+
+
+@dp.callback_query_handler(lambda callback: callback.data == 'expanse_button')
+async def get_budget(callback: types.CallbackQuery):
+    await callback.answer(cache_time=60)
+    await bot.send_message(
+        callback.from_user.id,
+        base.EXPANSE_MESSAGE_START
+    )
+    await ExpanseState.value.set()
+
+
+@dp.message_handler(state=ExpanseState.value)
+async def get_expanse_value(message: types.Message, state: FSMContext):
+    if message.text.isdigit():
+        async with state.proxy() as data:
+            data['value'] = message.text
+        await message.answer(
+            base.EXPANSE_MESSAGE_END,
+            reply_markup=expanse_category_keyboard
+        )
+        await ExpanseState.next()
+    else:
+        await message.answer(error.PARSE_INT_ERROR_MESSAGE)
+
+
+@dp.message_handler(state=ExpanseState.category)
+async def get_expanse_category(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['category'] = message.text
+    await message.answer(
+        base.YOUR_EXPANSE_MESSAGE.format(
+            value=data['value'],
+            category=data['category']
+        ),
+        reply_markup=menu_keyboard
+    )
+    await state.finish()
