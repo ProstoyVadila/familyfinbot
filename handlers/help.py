01723e10e88b77361b5bc1c08fd37b7360e979fb
gorbachev <vadim.gorbachev.main@yandex.ru>
2020-12-16 14:42:45 +0300

    refactored help hadler

--- handlers/help.py
+++ handlers/help.py
@@ -3,10 +3,16 @@ from aiogram.dispatcher.filters.builtin import CommandHelp
 
 from app import dp, bot
 from messages.base import HELP_MESSAGE, HELP_MESSAGE_COMMANDS
+from keyboards.inline.start_markups import back_to_menu_markup
 
 
 @dp.callback_query_handler(lambda callback: callback.data == 'about_button')
 async def get_help(callback: types.CallbackQuery):
-    help_message = HELP_MESSAGE + '\n'.join(HELP_MESSAGE_COMMANDS)
+    help_message = HELP_MESSAGE.format(
+        user_name=callback.from_user.first_name) + '\n'.join(HELP_MESSAGE_COMMANDS)
     await callback.answer(cache_time=60)
-    await bot.send_message(callback.from_user.id, help_message)
+    await bot.send_message(
+        callback.from_user.id,
+        help_message,
+        reply_markup=back_to_menu_markup
+    )
