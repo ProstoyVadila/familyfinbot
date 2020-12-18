79c6e8a3e1f3a847b657ac0d931dd3fb7f06297a
gorbachev <vadim.gorbachev.main@yandex.ru>
2020-12-18 18:10:11 +0300

    refactored menu and help handlers

--- handlers/help.py
+++ handlers/help.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandHelp
 
@@ -6,20 +8,18 @@ from messages.base import HELP_MESSAGE, HELP_MESSAGE_COMMANDS
 from keyboards.inline.start_markups import back_to_menu_markup
 
 
-@dp.callback_query_handler(lambda callback: callback.data == 'about_button')
-async def get_help(callback: types.CallbackQuery):
-    help_message = HELP_MESSAGE.format(
-        user_name=callback.from_user.first_name) + '\n'.join(HELP_MESSAGE_COMMANDS)
-    await callback.answer(cache_time=60)
-    await bot.send_message(
-        callback.from_user.id,
-        help_message,
-        reply_markup=back_to_menu_markup
-    )
-
-
 @dp.message_handler(CommandHelp())
-async def get_help(message: types.Message):
+@dp.callback_query_handler(lambda callback: callback.data == 'about_button')
+async def get_help(answer_object: Union[types.Message, types.CallbackQuery]):
     help_message = HELP_MESSAGE.format(
-        user_name=message.from_user.id) + '\n'.join(HELP_MESSAGE_COMMANDS)
-    await message.answer(help_message, reply_markup=back_to_menu_markup)
+        user_name=answer_object.from_user.id) + '\n'.join(HELP_MESSAGE_COMMANDS)
+
+    if isinstance(answer_object, types.Message):
+        await answer_object.answer(help_message, reply_markup=back_to_menu_markup)
+    else:
+        await answer_object.answer(cache_time=60)
+        await bot.send_message(
+            answer_object.from_user.id,
+            help_message,
+            reply_markup=back_to_menu_markup
+        )
--- handlers/start.py
+++ handlers/start.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
@@ -11,17 +13,16 @@ async def start_bot(message: types.Message):
     await message.answer(START_MESSAGE, reply_markup=start_markup)
 
 
-@dp.callback_query_handler(lambda callback: callback.data == 'menu_button')
-async def get_menu(callback: types.CallbackQuery = None):
-    await callback.answer(cache_time=60)
-    await bot.send_message(
-        callback.from_user.id,
-        MENU_MESSAGE,
-        reply_markup=menu_markup
-    )
-
-
-@dp.message_handler(lambda message: message.text == 'меню')
 @dp.message_handler(commands=['menu'])
-async def get_menu(message: types.Message):
-    await message.answer(MENU_MESSAGE, reply_markup=menu_markup)
+@dp.message_handler(lambda message: message.text == 'меню')
+@dp.callback_query_handler(lambda callback: callback.data == 'menu_button')
+async def get_menu(answer_object: Union[types.Message, types.CallbackQuery]):
+    if isinstance(answer_object, types.Message):
+        await answer_object.answer(MENU_MESSAGE, reply_markup=menu_markup)
+    else:
+        await answer_object.answer(cache_time=60)
+        await bot.send_message(
+            answer_object.from_user.id,
+            MENU_MESSAGE,
+            reply_markup=menu_markup
+        )
