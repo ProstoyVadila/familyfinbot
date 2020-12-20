58070a5cb9ac27dfa9b873c30e83055efb73cb0f
gorbachev <vadim.gorbachev.main@yandex.ru>
2020-12-20 17:16:01 +0300

    added update budget method for user but without converting

--- model/models/user.py
+++ model/models/user.py
@@ -1,3 +1,4 @@
+from aiogram.dispatcher.storage import FSMContextProxy
 from sqlalchemy import sql
 
 from model.db_gino import db, TimeBaseModel
@@ -8,7 +9,7 @@ class User(TimeBaseModel):
 
     user_id = db.Column(db.Integer(), primary_key=True, index=True)
     user_name = db.Column(db.Unicode())
-    user_budget = db.Column(db.Integer())
+    user_budget = db.Column(db.Float())
 
     query: sql.Select
 
@@ -18,3 +19,13 @@ class User(TimeBaseModel):
         if not user:
             return await cls.create(user_id=user_id, user_name=name)
         return user
+
+    @classmethod
+    async def update_budget(cls, user_id: int, value: float, period: str):
+        user = await cls.get_or_create(user_id)
+        await user.update(user_budget=value).apply()
+
+    @classmethod
+    async def get_budget(cls, user_id):
+        user = await cls.get(user_id)
+        print(f"VASH BUDGET {user.user_budget}")
--- utils/extractors.py
+++ utils/extractors.py
@@ -6,6 +6,7 @@ def parse_value(message_raw: str) -> Optional[float]:
     if message_raw.isdigit():
         return float(message_raw)
     message = re.sub(r'\s+', ' ', message_raw)
+    print(f'__________________{message}____________________')
     regexp = re.search(r'([.]?\d[\d\s.,]*)', message, re.VERBOSE)
     if regexp:
         value = regexp.group(0).replace(',', '.')
