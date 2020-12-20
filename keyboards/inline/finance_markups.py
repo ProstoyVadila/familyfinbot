c4b237241ec5731967793a48d87ddbc634a25de0
gorbachev <vadim.gorbachev.main@yandex.ru>
2020-12-21 02:31:49 +0300

    added emojiz to buttons

--- keyboards/inline/finance_markups.py
+++ keyboards/inline/finance_markups.py
@@ -1,4 +1,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import emoji as e
+
 
 budget_markup = InlineKeyboardMarkup(
     inline_keyboard=[
@@ -27,13 +29,15 @@ stats_markup = InlineKeyboardMarkup(
     inline_keyboard=[
         [
             InlineKeyboardButton(
-                text='Узнать баланс',
+                text='Узнать баланс  ' \
+                + e.emojize(':credit_card:'),
                 callback_data='balance_stats_button'
             )
         ],
         [
             InlineKeyboardButton(
-                text='График трат',
+                text='График трат  ' \
+                + e.emojize(':chart_increasing_with_yen:'),
                 callback_data='graphs_stats_button'
             )
         ]
@@ -44,13 +48,15 @@ back_stats_markup = InlineKeyboardMarkup(
     inline_keyboard=[
         [
             InlineKeyboardButton(
-                text='Вернуться назад',
+                text='Вернуться назад  ' \
+                + e.emojize(':BACK_arrow:'),
                 callback_data='statistics_button'
             )
         ],
         [
             InlineKeyboardButton(
-                text='Вернуться в меню',
+                text='Вернуться в меню  ' \
+                + e.emojize(':page_with_curl:'),
                 callback_data='menu_button'
             )
         ]
@@ -61,19 +67,22 @@ graphs_stats_markup = InlineKeyboardMarkup(
     inline_keyboard=[
         [
             InlineKeyboardButton(
-                text='Мои расходы по категориям',
+                text='Мои расходы по категориям  ' \
+                + e.emojize(':bar_chart:'),
                 callback_data='graph1_button'
             )
         ],
         [
             InlineKeyboardButton(
-                text='Мои доходы по категориям',
+                text='Мои доходы по категориям  ' \
+                + e.emojize(':bar_chart:'),
                 callback_data='graph2_button'
             )
         ],
         [
             InlineKeyboardButton(
-                text='Назад',
+                text='Назад  ' \
+                + e.emojize(':BACK_arrow:'),
                 callback_data='statistics_button'
             )
         ]
