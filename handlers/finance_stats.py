86fcb4b00755f3c967a2fd653cd7118fce88b6e2
gorbachev <vadim.gorbachev.main@yandex.ru>
2020-12-21 14:32:58 +0300

    refactored

--- handlers/finance_stats.py
+++ handlers/finance_stats.py
@@ -1,20 +1,17 @@
 from typing import Union
 
 from aiogram import types
-from aiogram.dispatcher import FSMContext
 
 from app import dp, bot
-from messages import finance, error
-from keyboards.finance_keyboard import menu_keyboard
+from messages import finance
 from keyboards.inline import finance_markups
-from states.finance_states import IncomeState
 
 
 GRAPHS_CALLBACK_DATA = ('graph1_button', 'graph2_button')
 
 
 @dp.message_handler(commands=['stats'])
-@dp.callback_query_handler(lambda callback: callback.data == 'statistics_button')
+@dp.callback_query_handler(lambda cb: cb.data == 'statistics_button')
 async def get_stats(types_object: Union[types.Message, types.CallbackQuery]):
     if isinstance(types_object, types.CallbackQuery):
         await types_object.answer(cache_time=60)
@@ -26,7 +23,7 @@ async def get_stats(types_object: Union[types.Message, types.CallbackQuery]):
 
 
 @dp.message_handler(commands=['balance'])
-@dp.callback_query_handler(lambda callback: callback.data == 'balance_stats_button')
+@dp.callback_query_handler(lambda cb: cb.data == 'balance_stats_button')
 async def get_balance(types_object: Union[types.Message, types.CallbackQuery]):
     if isinstance(types_object, types.CallbackQuery):
         await types_object.answer(cache_time=60)
@@ -39,7 +36,7 @@ async def get_balance(types_object: Union[types.Message, types.CallbackQuery]):
 
 
 @dp.message_handler(commands=['graphs'])
-@dp.callback_query_handler(lambda callback: callback.data == 'graphs_stats_button')
+@dp.callback_query_handler(lambda cb: cb.data == 'graphs_stats_button')
 async def get_graph(types_object: Union[types.Message, types.CallbackQuery]):
     if isinstance(types_object, types.CallbackQuery):
         await types_object.answer(cache_time=60)
@@ -51,7 +48,7 @@ async def get_graph(types_object: Union[types.Message, types.CallbackQuery]):
     )
 
 
-@dp.callback_query_handler(lambda callback: callback.data in GRAPHS_CALLBACK_DATA)
+@dp.callback_query_handler(lambda cb: cb.data in GRAPHS_CALLBACK_DATA)
 async def get_expanse_by_categories_graph(callback: types.CallbackQuery):
     await callback.answer(cache_time=60)
     await bot.send_message(
--- states/__init__.py
+++
@@ -1,1 +0,0 @@
-from .finance_states import BudgetState
