5158a8204e9a64340309aaac88b34b73b718d1eb
gorbachev <vadim.gorbachev.main@yandex.ru>
2020-12-23 18:09:10 +0300

    added commands to default bot commands

--- utils/default_commands.py
+++ utils/default_commands.py
@@ -3,6 +3,13 @@ from aiogram import types
 
 async def set_default_commands(dp):
     await dp.bot.set_my_commands([
-        types.BotCommand("start", "Запустить бота"),
+        types.BotCommand("balance", "Узнать баланс"),
+        types.BotCommand("expense", "Внести расход"),
+        types.BotCommand("income", "Внести доход"),
+        types.BotCommand("stats", "Получить аналитику"),
+        types.BotCommand("budget", "Установить бюджет"),
+        types.BotCommand("download", "Выгрузить свои операции"),
+        types.BotCommand("feedback", "Оставить фидбек"),
+        types.BotCommand("menu", "Меню"),
         types.BotCommand("help", "Помощь"),
     ])
